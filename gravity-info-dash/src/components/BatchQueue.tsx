8b0856837fffe5b557951e268bb78e29f22b1fe4
Pham Tu <tubackkhoa@gmail.com>
2023-02-08 14:35:10 +0700

    restructure layout

--- gravity-info-dash/src/components/BatchQueue.tsx
+++ gravity-info-dash/src/components/BatchQueue.tsx
@@ -18,13 +18,24 @@ interface Props {
   erc20Metadata: Array<Erc20Metadata>;
   ethBridgeInfo: EthInfo;
   etherscanBase: string;
+  evmChainPrefix: string;
 }
+
+const prefixToName = (prefix: string) => {
+  switch (prefix) {
+    case 'oraib':
+      return 'Binance Smart Chain';
+    default:
+      return 'Ethereum Mainnet';
+  }
+};
 
 export const BatchQueue: React.FC<Props> = ({
   gravityBridgeInfo,
   erc20Metadata,
   ethBridgeInfo,
-  etherscanBase
+  etherscanBase,
+  evmChainPrefix
 }) => {
   return (
     <Card className="ParametersCard" style={{ borderRadius: 8, padding: 20 }}>
@@ -32,80 +43,78 @@ export const BatchQueue: React.FC<Props> = ({
         <CardTitle tag="h1">Batch Queue</CardTitle>
         <CardSubtitle>
           These transactions are in batches and waiting to be relayed to
-          Ethereum
+          <strong> {prefixToName(evmChainPrefix)}</strong>
         </CardSubtitle>
         {getNotExecutedBatches(gravityBridgeInfo, ethBridgeInfo).map(
           (batch: TransactionBatch) => (
-            <Card key={batch.nonce}>
-              <CardBody>
-                <CardTitle tag="h5">
-                  {' '}
-                  Batch #{batch.nonce}{' '}
-                  {
-                    getMetadataFromList(batch.token_contract, erc20Metadata)
-                      ?.symbol
-                  }
-                </CardTitle>
-                <div>
-                  Total Fees:{' '}
-                  {amountToFraction(
-                    batch.token_contract,
-                    batch.total_fee.amount,
-                    erc20Metadata
-                  )}
-                </div>
-                <div>
-                  Timeout:{' '}
-                  <a href={`${etherscanBase}/block/${batch.batch_timeout}`}>
-                    {batch.batch_timeout}
-                  </a>
-                </div>
-                <Table
-                  dark
-                  borderless
-                  size="sm"
-                  responsive
-                  style={{ borderSpacing: 10, fontSize: 15 }}
-                >
-                  <thead>
-                    <tr>
-                      <th>To</th>
-                      <th>From</th>
-                      <th>Amount / Fee</th>
+            <div key={batch.nonce}>
+              <h5>
+                {' '}
+                Batch #{batch.nonce}{' '}
+                {
+                  getMetadataFromList(batch.token_contract, erc20Metadata)
+                    ?.symbol
+                }
+              </h5>
+              <div>
+                Total Fees:{' '}
+                {amountToFraction(
+                  batch.token_contract,
+                  batch.total_fee.amount,
+                  erc20Metadata
+                )}
+              </div>
+              <div className="mb-4">
+                Timeout:{' '}
+                <a href={`${etherscanBase}/block/${batch.batch_timeout}`}>
+                  {batch.batch_timeout}
+                </a>
+              </div>
+              <Table
+                dark
+                borderless
+                size="sm"
+                responsive
+                style={{ borderSpacing: 10, fontSize: 15 }}
+              >
+                <thead>
+                  <tr>
+                    <th>To</th>
+                    <th>From</th>
+                    <th>Amount / Fee</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {batch.transactions.map((batchTx: BatchTransaction) => (
+                    <tr key={batchTx.id}>
+                      <td>
+                        <a href={etherscanBase + batchTx.destination}>
+                          {batchTx.destination}
+                        </a>
+                      </td>
+                      <td>
+                        <a href={cosmosAddressToExplorerLink(batchTx.sender)}>
+                          {batchTx.sender}
+                        </a>
+                      </td>
+                      <td>
+                        {amountToFraction(
+                          batchTx.erc20_token.contract,
+                          batchTx.erc20_token.amount,
+                          erc20Metadata
+                        )}
+                        /
+                        {amountToFraction(
+                          batchTx.erc20_token.contract,
+                          batchTx.erc20_fee.amount,
+                          erc20Metadata
+                        )}
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {batch.transactions.map((batchTx: BatchTransaction) => (
-                      <tr key={batchTx.id}>
-                        <td>
-                          <a href={etherscanBase + batchTx.destination}>
-                            {batchTx.destination}
-                          </a>
-                        </td>
-                        <td>
-                          <a href={cosmosAddressToExplorerLink(batchTx.sender)}>
-                            {batchTx.sender}
-                          </a>
-                        </td>
-                        <td>
-                          {amountToFraction(
-                            batchTx.erc20_token.contract,
-                            batchTx.erc20_token.amount,
-                            erc20Metadata
-                          )}
-                          /
-                          {amountToFraction(
-                            batchTx.erc20_token.contract,
-                            batchTx.erc20_fee.amount,
-                            erc20Metadata
-                          )}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
           )
         )}
       </CardBody>
