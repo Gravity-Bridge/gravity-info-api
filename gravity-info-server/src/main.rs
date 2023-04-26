7444c6f84a4c6c5259c481803bdaa0119a8b3f1b
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2023-04-26 02:10:51 -0700

    Add Database & TX downloader

--- gravity-info-server/src/main.rs
+++ gravity-info-server/src/main.rs
@@ -5,6 +5,7 @@ pub mod gravity_info;
 pub mod tls;
 pub mod total_suppy;
 pub mod volume;
+pub mod transactions;
 
 const DEVELOPMENT: bool = cfg!(feature = "development");
 const SSL: bool = !DEVELOPMENT;
@@ -20,13 +21,19 @@ use crate::total_suppy::get_supply_info;
 use crate::volume::get_volume_info;
 use crate::{gravity_info::get_gravity_info, tls::*};
 use actix_cors::Cors;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
 use gravity_info::{blockchain_info_thread, get_eth_info};
 use log::info;
 use rustls::ServerConfig;
 use total_suppy::chain_total_supply_thread;
 use volume::bridge_volume_thread;
+use transactions::{transactions, ApiResponse, CustomMsgSendToEth};
+use std::sync::Arc;
+use rocksdb::DB;
+use rocksdb::Options;
+use tokio::runtime::Runtime;
+
 
 #[get("/total_supply")]
 async fn get_total_supply() -> impl Responder {
@@ -98,10 +105,44 @@ async fn get_bridge_volume() -> impl Responder {
     }
 }
 
+#[get("/transactions")]
+async fn get_all_msg_send_to_eth_transactions(db: web::Data<Arc<DB>>) -> impl Responder {
+    let mut response_data = Vec::new();
+
+    let iterator = db.iterator(rocksdb::IteratorMode::Start);
+
+    for item in iterator {
+        match item {
+            Ok((key, value)) => {
+                let key_str = String::from_utf8_lossy(&key);
+                if key_str.starts_with("msgSendToEth_") {
+                    let msg_send_to_eth: CustomMsgSendToEth = serde_json::from_slice(&value).unwrap();
+                    println!("Queried MsgSendToEth: {:?}", msg_send_to_eth);
+                    response_data.push(ApiResponse {
+                        tx_hash: key_str.replace("msgSendToEth_", ""),
+                        data: serde_json::to_value(&msg_send_to_eth).unwrap(),
+                    });
+                }
+            }
+            Err(err) => {
+                eprintln!("RocksDB iterator error: {}", err);
+            }
+        }
+    }
+    response_data.sort_by(|a, b| a.tx_hash.cmp(&b.tx_hash));
+    HttpResponse::Ok().json(response_data)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     openssl_probe::init_ssl_cert_env_vars();
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+     // starts a background thread for downloading transactions
+     let mut db_options = Options::default();
+     db_options.create_if_missing(true);
+     let db = Arc::new(DB::open(&db_options, "transactions").expect("Failed to open database"));
+     let api_db = web::Data::new(db.clone());
+     transactions(api_db.clone(), db.clone(), &db_options);
     // starts background thread for gathering into
     blockchain_info_thread();
     // starts a background thread for generating the total supply numbers
@@ -109,7 +150,7 @@ async fn main() -> std::io::Result<()> {
     // starts a background thread for generating volume numbers
     bridge_volume_thread();
 
-    let server = HttpServer::new(|| {
+    let server = HttpServer::new(move || {
         App::new()
             .wrap(
                 Cors::default()
@@ -124,6 +165,8 @@ async fn main() -> std::io::Result<()> {
             .service(get_gravity_bridge_info)
             .service(erc20_metadata)
             .service(get_bridge_volume)
+            .app_data(api_db.clone())
+            .service(get_all_msg_send_to_eth_transactions)
     });
 
     let server = if SSL {
@@ -144,4 +187,5 @@ async fn main() -> std::io::Result<()> {
     server.run().await?;
 
     Ok(())
+
 }
